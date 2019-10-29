4d8e29d21dea3e097a66e7d3af4f27a60ef52c90
Ritu Joshi <ritupandey1@gmail.com>
2019-10-28 20:39:12 -0400

    Added PARRK and some more changes

--- src/views/Header.php
+++ src/views/Header.php
@@ -1,7 +1,7 @@
 <header class="site-header js-site-header">
    <div class="container-fluid">
    <div class="row align-items-center">
-      <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="index.html">Hotel</a></div>
+      <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="index.html">PARRK Hotel</a></div>
       <div class="col-6 col-lg-8">
          <!-- <div class="site-login-toggle js-site-login-toggle" data-aos="fade">
             <i class="fas fa-user"></i>
@@ -44,7 +44,7 @@
             <span class="custom-caption text-uppercase text-white d-block  mb-3">Welcome To 5 <span class="fa fa-star text-primary"></span>   Hotel</span>         
             <?php 
                $page = ( isset($_GET['page']) ) ? $_GET['page'] : 'search';
-               $pageContent = array("search"=>"Best Place to stay", "rooms"=>"Room Details", "reservation"=>"Reservation", "about" => "About","registration"=>"registration","admin"=>"Administration","login"=>"login");               
+               $pageContent = array("search"=>"Best Place to stay", "rooms"=>"Room Details", "reservation"=>"Reservation", "about" => "About","registration"=>"Registration","admin"=>"Administration","login"=>"login");               
                echo "<h1 class='heading'>". $pageContent[$page] ."</h1>";
                ?>
          </div>
--- src/views/dropemployee.php
+++ src/views/dropemployee.php
@@ -1,33 +1,38 @@
-<h4>Employees</h4>
-  <table border="1">
-    <thead>
-      <tr class="row100 head">
-        <th class="cell100 column1">Employee Name</th>
-        <th class="cell100 column2">Employee ID</th>
-        <th class="cell100 column3">Date of Birth</th>
-        <th class="cell100 column3">Start date</th>
-        <th class="cell100 column4">Department</th>
-        <th class="cell100 column5">Contact Number</th>
-      </tr>
-    </thead>
-    <tbody>
-    <?php
-      $conn = mysqli_connect("localhost", "root", "", "CRMDB_SE");
-      // Check connection
-      if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
-      $sql = "SELECT E_FName,E_MName,E_LName,E_Id,Dept_Name,DOB,E_Id,Contact,E_Start_Date from employee where emp_status = 'Active'";
-      $result = $conn->query($sql);
+<section >
+  <div class="container">
+    <div class="row">
+        <table class="table table-bordered">
+          <thead>
+            <tr class="row100 head">
+              <th class="cell100 column1">Employee Name</th>
+              <th class="cell100 column2">Employee ID</th>
+              <th class="cell100 column3">Date of Birth</th>
+              <th class="cell100 column3">Start date</th>
+              <th class="cell100 column4">Department</th>
+              <th class="cell100 column5">Contact Number</th>
+            </tr>
+          </thead>
+          <tbody>
+          <?php
+            $conn = mysqli_connect("localhost", "root", "", "CRMDB_SE");
+            // Check connection
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+            $sql = "SELECT E_FName,E_MName,E_LName,E_Id,Dept_Name,DOB,E_Id,Contact,E_Start_Date from employee where emp_status = 'Active'";
+            $result = $conn->query($sql);
 
-      if ($result->num_rows > 0) {
-      // output data of each row
-        while($row = $result->fetch_assoc()) {
-          echo "<tr class='row100 body'><td>" . $row["E_FName"]. "</td><td>" . $row["E_Id"] . "</td><td>" . $row["DOB"] ."</td><td>". $row["E_Start_Date"]. "</td><td>". $row["Dept_Name"] . "</td><td>". $row["Contact"] . "</td></tr>";
-        }
-        echo "</tbody></table>";
-        //echo "<a href='UserReport_Export.php'> Export To Excel </a>";
-        } else { echo "0 results"; }
-      $conn->close();
-      ?>
-      <button id="drop" class="btn btn-primary text-white font-weight-bold">Drop Employee</button>   
+            if ($result->num_rows > 0) {
+            // output data of each row
+              while($row = $result->fetch_assoc()) {
+                echo "<tr class='row100 body'><td>" . $row["E_FName"]. "</td><td>" . $row["E_Id"] . "</td><td>" . $row["DOB"] ."</td><td>". $row["E_Start_Date"]. "</td><td>". $row["Dept_Name"] . "</td><td>". $row["Contact"] . "</td></tr>";
+              }
+              echo "</tbody></table>";
+              //echo "<a href='UserReport_Export.php'> Export To Excel </a>";
+              } else { echo "0 results"; }
+            $conn->close();
+          ?>
+          <button id="drop" class="btn btn-primary text-white font-weight-bold">Drop Employee</button>          
+      </div>
+  </div> 
+</section>
