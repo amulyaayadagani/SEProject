3e58c7a049142e518bdbd25b126b7516f0affb09
Ritu Joshi <ritupandey1@gmail.com>
2019-11-12 10:53:37 -0500

    added addemployee

--- src/views/addemployee.php
+++ src/views/addemployee.php
@@ -10,27 +10,27 @@
               <div class="row">
                 <div class="col-md-4 form-group">
                   <label for="emp_fname">First Name</label>
-                  <input type="text" id="emp_fname" class="form-control " required>
+                  <input type="text" id="emp_fname" name="emp_fname" class="form-control " required>
                 </div>
                 <div class="col-md-4 form-group">
                   <label for="emp_mname">Middle Name</label>
-                  <input type="text" id="emp_mname" class="form-control ">
+                  <input type="text" id="emp_mname" name="emp_mname" class="form-control ">
                 </div>
                 <div class="col-md-4 form-group">
                   <label for="emp_lname">Last Name</label>
-                  <input type="text" id="emp_lname" class="form-control " required>
+                  <input type="text" id="emp_lname" name="emp_lname" class="form-control " required>
                 </div>                  
               </div>
               <div class="row" diabled=true>
                 <div class="col-md-4 form-radio">
                   <label for="gender" class="radio-label">Gender :</label>
                   <div class="form-radio-item">
-                      <input type="radio" name="gender" id="male" checked>
+                      <input type="radio" name="gender" id="male" value="Male" checked>
                       <label for="male">Male</label>
                       <span class="check"></span>
                   </div>
                   <div class="form-radio-item">
-                      <input type="radio" name="gender" id="female">
+                      <input type="radio" name="gender" id="female" value="Female">
                       <label for="female">Female</label>
                       <span class="check"></span>
                   </div>
@@ -43,13 +43,13 @@
               <div class="row">
                 <div class="col-md-12 form-group">
                   <label for="phone">Phone</label>
-                  <input type="text" id="phone" class="form-control ">
+                  <input type="text" id="phone" name="phone" class="form-control ">
                 </div> 
               </div>
               <div class="row">
                 <div class="col-md-12 form-group">
                   <label for="email">Email</label>
-                  <input type="email" id="email" class="form-control ">
+                  <input type="email" id="email" name="email" class="form-control" required>
                 </div>
               </div>
               <div class="row">
@@ -58,7 +58,7 @@
                   <input type="text" name="address" id="address" class="form-control" />
               </div>
             </div>
-            <div class="row">
+            <div class="row" style="display: none">
               <div class="col-md-6 mb-3 mb-md-0">
                 <label for="city" >City</label>
                 <div class="field-icon-wrap">
@@ -83,7 +83,7 @@
             </div>
             <div class="row">
               <div class="col-md-6 form-group">
-                <input type="submit" value="Save" class="btn btn-primary text-white font-weight-bold">
+                <input type="submit" name="add_emp_btn" id="add_emp_btn" value="Add Employee" class="btn btn-primary text-white font-weight-bold">
               </div>
             </div>
             </form>
@@ -93,3 +93,27 @@
     <!--</section> -->
   </div>
    <!-- </section> -->
+  <?php
+   //print_r($_POST);
+      if(isset($_POST["emp_fname"]) && isset($_POST["emp_mname"]) && isset($_POST["emp_lname"]) && isset($_POST["birth_date"]) && isset($_POST["phone"]) && isset($_POST["email"]) && isset($_POST["address"])) 
+    {
+      //echo "inside";
+      $conn = mysqli_connect("localhost", "root", "", "CRMDB_SE");
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+      $sql = "Insert into employee(E_FName,E_LName,E_MName,address,Contact,email,DOB,gender) values('" . $_POST["emp_fname"] . "','" . $_POST["emp_lname"] . "','" . $_POST["emp_mname"]  . "','" . $_POST["address"] . "'," . $_POST["phone"] . ",'" . $_POST["email"] . "','" . $_POST["birth_date"] . "','" . $_POST["gender"] . "')";
+      //echo $sql;
+      if($conn->query($sql) == true)
+      {
+        //$_SESSION["message"] = "";
+        echo "Record inserted successfully";
+        
+      }
+      else{
+        echo "Error inserting record: " . $conn->error;
+      }
+      $conn->close();
+    }
+   ?>
